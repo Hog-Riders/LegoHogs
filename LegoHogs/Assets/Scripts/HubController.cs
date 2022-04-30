7183fe0df9290512a9b8fe37f1143bea1a07c646
Florian Schut <mail@florianschut.nl>
2022-04-30 02:32:00 +0200

    Controllable planes with a ball

---
+++ LegoHogs/Assets/Scripts/HubController.cs
@@ -0,0 +1,68 @@
+using TMPro;
+using UnityEngine;
+
+public class HubController : MonoBehaviour
+{
+    [SerializeField] HubBase blueHub;
+    [SerializeField] HubBase yellowHub;
+    //[SerializeField] TMP_Text output;
+    [SerializeField] Transform blueHubModel;
+    [SerializeField] Transform yellowHubModel;
+    //[SerializeField] Transform hubButton;
+    //[SerializeField] ParticleSystem hubButtonEffect;
+    //[SerializeField] Transform xAxisPositive;
+    //[SerializeField] Transform xAxisNegative;
+    //[SerializeField] Transform yAxisPositive;
+    //[SerializeField] Transform yAxisNegative;
+    //[SerializeField] Transform zAxisPositive;
+    //[SerializeField] Transform zAxisNegative;
+
+    //public void OnIsConnectedChanged(bool connected)
+    //{
+    //    output.text = connected ? "Connected" : "Disconnected";
+    //}
+
+    //public void OnButtonChanged(bool pressed)
+    //{
+    //    hubButton.localPosition = new Vector3(0f, pressed ? 1.81f: 2f, 1.6f);
+    //    if (pressed)
+    //    {
+    //        hubButtonEffect.Emit(20);
+    //    }
+    //}
+
+    //public void OnAccelerationChanged(Vector3 acceleration)
+    //{
+    //    xAxisPositive.gameObject.SetActive(acceleration.x > 0.01f);
+    //    xAxisNegative.gameObject.SetActive(acceleration.x < -0.01f);
+    //    xAxisPositive.localScale = new Vector3(100f, 100f, 1000f * Mathf.Max(0f, acceleration.x));
+    //    xAxisNegative.localScale = new Vector3(100f, 100f, 1000f * Mathf.Max(0f, -acceleration.x));
+
+    //    yAxisPositive.gameObject.SetActive(acceleration.y > 0.01f);
+    //    yAxisNegative.gameObject.SetActive(acceleration.y < -0.01f);
+    //    yAxisPositive.localScale = new Vector3(100f, 100f, 1000f * Mathf.Max(0f, acceleration.y));
+    //    yAxisNegative.localScale = new Vector3(100f, 100f, 1000f * Mathf.Max(0f, -acceleration.y));
+
+    //    zAxisPositive.gameObject.SetActive(acceleration.z > 0.01f);
+    //    zAxisNegative.gameObject.SetActive(acceleration.z < -0.01f);
+    //    zAxisPositive.localScale = new Vector3(100f, 100f, 1000f * Mathf.Max(0f, acceleration.z));
+    //    zAxisNegative.localScale = new Vector3(100f, 100f, 1000f * Mathf.Max(0f, -acceleration.z));
+    //}
+
+    public void OnBlueOrientationChanged(Vector3 orientation)
+    {
+        blueHubModel.rotation = Quaternion.Euler(orientation);
+    }
+    public void OnYellowOrientationChanged(Vector3 orientation)
+    {
+        yellowHubModel.rotation = Quaternion.Euler(orientation);
+    }
+
+    void Update()
+    {
+        //if (hub.IsConnected)
+        //{
+        //    output.text = "Battery " + hub.BatteryLevel + "%";
+        //}
+    }
+}
