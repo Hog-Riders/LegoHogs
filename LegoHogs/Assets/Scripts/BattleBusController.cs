f61fc160f323d33608d34a309f3a2c8fe76639b1
Jasper Drescher <jasperdre@gmail.com>
2022-05-01 13:43:20 +0200

    Change name and add cursor

--- LegoHogs/Assets/Scripts/BattleBusController.cs
+++ LegoHogs/Assets/Scripts/BattleBusController.cs
@@ -1,10 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
-using UnityEditor.UIElements;
 using UnityEngine;
-using Quaternion = UnityEngine.Quaternion;
-using Vector3 = UnityEngine.Vector3;
 
 public class BattleBusController : MonoBehaviour
 {
